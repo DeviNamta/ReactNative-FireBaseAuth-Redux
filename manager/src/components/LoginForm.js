f79f926916fbcaca164652258a4625217ab2fc3b
Namta Devi <harjotsingh@Harjots-MacBook-Air.local>
2018-06-07 14:43:44 -0700

    commit

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component  } from 'react';
+import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged,passwordChanged,loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
     onEmailChange(text) {
@@ -16,6 +17,28 @@ class LoginForm extends Component {
         const { email, password } = this.props;
         this.props.loginUser({ email, password });
     }
+
+    renderError() {
+        if(this.props.error){
+            return (
+                <View style={{backgroundColor: 'white'}}>
+                    <Text style={styles.errorTextStyle}>
+                    {this.props.error}
+                    </Text>
+                </View>
+            );
+        }
+    }
+
+    renderButton() {
+        if(this.props.loading) {
+            return <Spinner size="large" />;
+        }
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
+        );
+    }
+
     render() {
         return (
             <Card>
@@ -36,18 +59,30 @@ class LoginForm extends Component {
                         value={this.props.password}
                     />
                 </CardSection>
+                {this.renderError()}
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
     }
 }
 
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+}
+
 const mapStateToProps = state => {
     return {
         email: state.auth.email,
-        password: state.auth.password
+        password: state.auth.password,
+        error: state.auth.error,
+        loading: state.auth.loading
+        
     };
 };
 
